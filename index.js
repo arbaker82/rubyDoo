127c88554b95c7311c5f22231654f1c405afe197
adam.baker <abaker82@gmail.com>
2024-04-18 21:30:54 -0400

    img gallery scaling

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Select all images in the gallery
+    const images = document.querySelectorAll('.gallery img');
 
-  
+    // Add click event listener to each image
+    images.forEach(image => {
+        image.addEventListener('click', () => {
+            // Open the image in a new tab
+            window.open(image.src, '_blank');
+        });
+    });
+});
