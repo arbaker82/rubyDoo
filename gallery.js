932e6e3c2858473a72e025469fede24fbe5a49ca
Adam Baker <148913138+arbaker82@users.noreply.github.com>
2024-04-22 12:51:26 -0400

    got image gallery/carousel running

--- gallery.js
+++ gallery.js
@@ -5,16 +5,29 @@ document.addEventListener('DOMContentLoaded', function () {
     let currentIndex = 0;
 
     function showImage(index) {
-        images.forEach((image,idx)) => {
-            if (idx=== index) {
-                image.style.display = 'block'
-            } else {image.style.display = 'none'};
-
+        images.forEach((image,idx) => {
+            if (idx === index) {
+                image.style.display = 'block';
+            } else {
+                image.style.display = 'none';
+        
         }
     });
 }
 
     showImage(currentIndex);
 
-    function showNextImage () {}
+    function showNextImage () {
+        currentIndex = (currentIndex + 1) % images.length;
+        showImage(currentIndex);
+    }
 
+    function showPrevImage() {
+        currentIndex = (currentIndex - 1 + images.length) % images.length;
+        showImage(currentIndex);
+    }
+    nextBtn.addEventListener('click', showNextImage);
+    prevBtn.addEventListener('click', showPrevImage);
+
+    setInterval(showNextImage, 30000);
+});
