b0decb082a6b1c0c920c5a2119124a55b866b215
Adam Baker <148913138+arbaker82@users.noreply.github.com>
2024-04-25 11:22:18 -0400

    trying to add imgslider to index with js

---
+++ index.js
@@ -0,0 +1,21 @@
+let currentIndex = 0;
+
+const slides = document.querySelectorAll("#slider img");
+
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    slides.forEach((slide) => {
+        slide.style.display = "none";
+    });
+    slides[index].style.display = "block";
+
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % totalSlides;
+    showSlide(currentIndex);
+
+}
+
+setInterval(nextSlide, 5000);
